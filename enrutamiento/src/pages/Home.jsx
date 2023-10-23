c67fab40cfcd1de3e685a825d0aaa5e03fbc15a0
Carlos Gutierrez <cagutierrezr.17@icloud.com>
2023-10-22 21:54:54 -0300

    Correcciones 2
    correciones 2

--- enrutamiento/src/pages/Home.jsx
+++ enrutamiento/src/pages/Home.jsx
@@ -1,21 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home';
-import Numero from './pages/Numero';
-import Palabra from './pages/Palabra';
-import './Componentes/Router.css'
+const Home = () => {
 
-const App = () => {
 	return (
-		<div className='App'>
-			<Router>
-				<Routes>
-					<Route path='/' element={<Home />} />
-					<Route path='/:valor' element={<Numero />} />
-					<Route path='/:valor/:color/:backgroundColor' element={<Palabra />} />
-				</Routes>
-			</Router>
-		</div>
-	);
+			<h1>Bienvenidos</h1>
+	)
 }
 
-export default App;
+export default Home;
